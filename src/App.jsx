3ac13e72268713190868d7303b8a3cd77b8b1f54
Benson Kiai <benson.kiai86@gmail.com>
2023-08-30 00:26:53 +0300

    Add basic Missions component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Myprofile from './components/Routes/Myprofile';
 import Rockets from './components/Routes/Rockets';
+import Missions from './components/Routes/Mission';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Rockets />} />
-        <Route path="Missions" />
+        <Route path="/Missions" element={<Missions />} />
         <Route path="/Profile" element={<Myprofile />} />
       </Routes>
     </>
