5bc61d50e2ba1f38406610308318351531c01386
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-08-29 14:19:02 -0300

    Create Rocket and Profile tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
+import Myprofile from './components/Routes/Myprofile';
+import Rockets from './components/Routes/Rockets';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-    </>
+      <Routes>
+        <Route path="/Profile" element={<Myprofile />} />
+        <Route path="/" element={<Rockets />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
 );
