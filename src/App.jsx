764f2562693afb1d92fb10dbcba5e0f55d4726d3
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-08-29 15:18:17 -0300

    fetchdata from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Myprofile from './components/Routes/Myprofile';
 import Rockets from './components/Routes/Rockets';
+import { setRockets } from './redux/rocketsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Fetch data from API and dispatch to Redux store
+    fetch('https://api.spacexdata.com/v3/rockets')
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(setRockets(data));
+      });
+  }, [dispatch]);
+
   return (
     <Router>
       <NavBar />
--- src/components/Routes/Rockets.jsx
+++ src/components/Routes/Rockets.jsx
@@ -1,6 +1,24 @@
+import { useSelector } from 'react-redux';
+
 function Rockets() {
+  const rockets = useSelector((state) => state.rockets);
+
   return (
-    <div><h2>This is the Rockets tab</h2></div>
+    <div>
+      <h2>Rockets</h2>
+      <ul>
+        {rockets.map((rocket) => (
+          <li key={rocket.id}>
+            <h3>{rocket.rocket_name}</h3>
+            <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+            <p>
+              Engine Type:
+              {rocket.engines.type}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
