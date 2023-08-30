816afd9a1c990a71b87a465047feccc8a36d27ad
Benson Kiai <benson.kiai86@gmail.com>
2023-08-30 20:39:58 +0300

    Store fetched missions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './rocketsSlice';
+import missionReducer from './missionSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: missionReducer,
   },
 });
 
