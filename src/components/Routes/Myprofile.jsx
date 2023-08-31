8a2b67971d84e94dd981cd3d461734cb61a76318
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-08-31 13:15:09 -0300

    created Myprofile list

--- src/components/Routes/Myprofile.jsx
+++ src/components/Routes/Myprofile.jsx
@@ -1,6 +1,26 @@
+import {
+  Col, Row, Container, ListGroup,
+} from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
 function Myprofile() {
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
-    <div><h2>This is Myprofile tab</h2></div>
+    <Container>
+      <Row>
+        <Col>
+          <h3>My Missions</h3>
+        </Col>
+        <Col>
+          <h3>My rockets</h3>
+          <ListGroup>
+            {reservedRockets.map((rocket) => (
+              <ListGroup.Item key={rocket.id}>{rocket.rocket_name}</ListGroup.Item>))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
