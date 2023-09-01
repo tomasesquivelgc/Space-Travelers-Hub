10f445d23266c9a1094e3d5c0625f5fd12fc5b3a
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-01 17:16:52 -0300

    add tests for rocketsSlice

---
+++ src/redux/rocketsSlice.test.js
@@ -0,0 +1,82 @@
+// tests for the rocketsSlice redux slice
+import rocketsReducer, { setRockets, reserveRocket, cancelRocket } from './rocketsSlice';
+import { expect, test } from "vitest";
+
+describe('reducers', () => {
+  test('should handle setRockets', () => {
+    const initialState = [];
+    const rockets = [
+      {
+        id: 'falcon1',
+        rocket_name: 'Falcon 1',
+        flickr_images: ['https://imgur.com/DaCfMsj.jpg'],
+        engines: {
+          type: 'merlin',
+        },
+        description: 'description',
+        reserved: false,
+      },
+    ];
+    const action = setRockets(rockets);
+    const nextState = rocketsReducer(initialState, action);
+    expect(nextState).toEqual(rockets);
+  })
+  test('should handle reserveRocket', () => {
+    const initialState = [
+      {
+        id: 'falcon1',
+        rocket_name: 'Falcon 1',
+        flickr_images: ['https://imgur.com/DaCfMsj.jpg'],
+        engines: {
+          type: 'merlin',
+        },
+        description: 'description',
+        reserved: false,
+      },
+      {
+        id: 'falcon9',
+        rocket_name: 'Falcon 9',
+        flickr_images: ['https://imgur.com/DaCfMsj.jpg'],
+        engines: {
+          type: 'merlin',
+        },
+        description: 'description',
+        reserved: false,
+      },
+    ];
+    const rocketId = 'falcon1';
+    const action = reserveRocket(rocketId);
+    const nextState = rocketsReducer(initialState, action);
+    expect(nextState[0].reserved).toBe(true);
+    expect(nextState[1].reserved).toBe(false);
+  });
+  test('should handle cancelRocket', () => {
+    const initialState = [
+      {
+        id: 'falcon1',
+        rocket_name: 'Falcon 1',
+        flickr_images: ['https://imgur.com/DaCfMsj.jpg'],
+        engines: {
+          type: 'merlin',
+        },
+        description: 'description',
+        reserved: true,
+      },
+      {
+        id: 'falcon9',
+        rocket_name: 'Falcon 9',
+        flickr_images: ['https://imgur.com/DaCfMsj.jpg'],
+        engines: {
+          type: 'merlin',
+        },
+        description: 'description',
+        reserved: false,
+      },
+    ];
+    const rocketId = 'falcon1';
+    const action = cancelRocket(rocketId);
+    const nextState = rocketsReducer(initialState, action);
+    expect(nextState[0].reserved).toBe(false);
+    expect(nextState[1].reserved).toBe(false);
+  });
+});
